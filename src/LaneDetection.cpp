267faa550f4a100a33e70a1251b8e986a25fe9f3
Orion <orionserup@gmail.com>
2020-12-18 21:06:01 -0800

    Fixed Errors in Lane Detection

--- src/LaneDetection.cpp
+++ src/LaneDetection.cpp
@@ -1,6 +1,9 @@
 #include "LaneDetection.hpp"
 
-ostream& operator<<(ostream& os, const igv::Lane& lane){
+using namespace igv;
+
+// Stream operator overload, allows you to print Lanes
+ostream& operator<<(ostream& os, const Lane& lane){
     
     os << "Slope: " << lane.slope;
     os << (lane.slope == 0.0f) ? " Y-Intercept: " : " X-Intercept: ";
@@ -18,54 +21,53 @@ ostream& operator<<(ostream& os, const igv::Lane& lane){
  * 
  */
 
-uint32_t igv::DetectLanes(array<igv::Lane, 2>& LaneArray, Mat& image){
+uint32_t DetectLanes(array<Lane, 2>& LaneArray, Mat& image){
 
     vector<Vec2f> linesP;  // a vector to fill with line points
-
     HoughLinesP(image, linesP, 1, CV_PI/256, 50, 25, 10.0f);  // find the lines
 
     if(linesP.size() < 2) return 0; // if there is less than one line return 0 lanes
-
-    double mag = 0;  // magnitude of the lines storage
-
-    pair<double, uint32_t> largest[2] = {{0,0}, {0,0}};  // stores index and magnitiude of largest lines
-
+    
+    // store index and magnitiude of largest lines and a temporary magnitude
+    struct {double mag, uint32_t index} largest = {{0.0f,0}, {0.0f,0}};  
+    double mag;
+    
     for( uint32_t i = 0; i < linesP.size(); i+=2){  // find the two largest lines
 
-        mag = linesP[i+i][x] - linesP[i][x] + linesP[i+1][y] - linesP[i][y];  // adds deltax and deltay
+        mag = linesP[i+i][x] - linesP[i][x] + linesP[i+1][y] - linesP[i][y];  
 
-        if(mag > largest[0].first) largest[0] = {mag, i}; // if larger than the largest then replace it
-        else if(mag > largest[i].first) largest[1] = {mag, i};  // same here except second largest
-
+        if(mag > largest[0].mag) 
+            largest[0] = {mag, i}; // if larger than the largest then replace it
+        else if(mag > largest[1].mag) 
+            largest[1] = {mag, i}; // same here except second largest
     }
 
-    double slope;
-    uint32_t intercept;
+    double slope; // temporary slope value
+    int intercept; // temporary intercept value
 
-    for( int i = 0; i < 2; i++){  // put the two biggest lines in the Lane Vector
+    for(uint32_t i = 0; i < 2; i++){  // put the two biggest lines in the Lane Vector 
 
-        if(linesP[largest[i].second][x] == linesP[largest[i].second + 1][x]){ // if vertical slope is max and xintercept is xintercpt 
+        if(linesP[largest[i].index][x] == linesP[largest[i].index + 1][x]){ // horizontal 
             
             slope = DBL_MAX;
-            intercept = linesP[largest[i].second][x];
-
+            intercept = linesP[largest[i].index][x];
         }
-        else if(linesP[largest[i].second][y] == linesP[largest[i].second][y]){
+        else if(linesP[largest[i].index][y] == linesP[largest[i].index + 1][y]){ // vertical
 
             slope = 0.0f;
-            intercept = linesP[largest[i].second][y];
+            intercept = linesP[largest[i].index][y];
         }
         else{
-
-        slope = (linesP[largest[i].second + 1][y] - linesP[largest[i].second][y])  // slope  = deltay/deltax
-                / (linesP[largest[i].second + 1][x] - linesP[largest[i].second][x]);
-        intercept = (uint32_t)(linesP[largest[1].second][x] - linesP[largest[i].second][x]/slope); // derived from point slope formula
-
+        // m = deltay/deltax
+        slope = (linesP[largest[i].index + 1][y] - linesP[largest[i].index][y]) 
+                    / (linesP[largest[i].index + 1][x] - linesP[largest[i].index][x]);
+        // b = Yo - mXo
+        intercept = (int)(linesP[largest[i].index][x] - slope*linesP[largest[i].index][x]); 
         }
-        
-        LaneArray[i] = { slope, intercept };  // fill in the lanes with their slopes and intercepts
+
+        LaneArray[i].slope = slope;
+        LaneArray[i].intercept = intercept;
     }
 
     return linesP.size()/2;  // return how many lines are seen
-
 }
